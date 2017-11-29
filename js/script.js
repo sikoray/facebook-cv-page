d229cd9920ffe35ea70f58e51b25f5ea55559256
sikoray <sikora.y@nton.info>
2017-11-30 00:53:19 +0200

    first

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+   $('.tabgroup > div').hide();
+$('.tabgroup > div:first-of-type').show();
+$('.tabs a').click(function(e){
+  e.preventDefault();
+    var $this = $(this),
+        tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
+        others = $this.closest('.tab').siblings().children('a'),
+        target = $this.attr('href');
+    others.removeClass('active');
+    $this.addClass('active');
+    $(tabgroup).children('div').hide();
+    $(target).show();
+  
+})
+
+$(document).ready(function(){
+    $("#flip, #panel, #messages").click(function(){
+        $("#panel").slideToggle("fast");
+    });
+});
+
+
+});
